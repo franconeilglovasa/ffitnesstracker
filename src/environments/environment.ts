0f1763e3e021f3b294bdd4b6dbf6efaa3449c69e
Franco Neil Glovasa <franconeilglovasa@outlook.com>
2021-08-02 19:04:02 +0800

    Section 6 File 2

---
+++ src/environments/environment.ts
@@ -0,0 +1,17 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyDs4yceIafdHFZVRF3vTjmukQV52kn1ZIY',
+  authDomain: 'ng-fitness-tracker-101a3.firebaseapp.com',
+  projectId: 'ng-fitness-tracker-101a3',
+  storageBucket: 'ng-fitness-tracker-101a3.appspot.com',
+  messagingSenderId: '839011876260',
+  appId: '1:839011876260:web:43a2d018455d94c2ead531',
+  measurementId: 'G-55S8W1F8TE'
+  }
+};
